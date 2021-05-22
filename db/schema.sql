0b5e6b293d6e9f659fe4e045521c67bd7a299279
Richard Crippen <richard.crippen@gmail.com>
2021-05-22 11:20:02 -0400

    setting up dependencies, connection, server, gitignore, and schema files

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+
+/* dropping any existing database */
+DROP DATABASE IF EXISTS cardcommerce_db;
+
+/* creating a new database */
+CREATE DATABASE cardcommerce_db;
+
+/* using the newly created database*/
+USE DATABASE cardcommerce_db; 
